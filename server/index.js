d26de0f630b5751ed1ac9b90208877b603e711aa
20A91A04D9 <84526643+20A91A04D9@users.noreply.github.com>
2023-04-25 10:06:54 +0530

    change in mlink

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const cors = require('cors')
 
 
-mongoose.connect('mongodb+srv://Trinath19:Naga%408656@cap.irwmo1m.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://naga:naga8656@caps.re10nkv.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
